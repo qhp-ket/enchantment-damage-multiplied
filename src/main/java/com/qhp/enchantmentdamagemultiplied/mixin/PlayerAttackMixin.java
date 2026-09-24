2de87ddadce2a05b73a377325c13c95583447097
qhp-ket <139011599+qhp-ket@users.noreply.github.com>
2026-09-25 01:29:53 +0800

    Package refmap with MixinExtras jar

--- src/main/java/com/qhp/enchantmentdamagemultiplied/mixin/PlayerAttackMixin.java
+++ src/main/java/com/qhp/enchantmentdamagemultiplied/mixin/PlayerAttackMixin.java
@@ -1,17 +1,14 @@
 package com.qhp.enchantmentdamagemultiplied.mixin;
 
 import com.qhp.enchantmentdamagemultiplied.EnchantmentDamageScaler;
-import net.minecraft.world.entity.MobType;
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.enchantment.EnchantmentHelper;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(Player.class)
 public abstract class PlayerAttackMixin {
-    @Redirect(
+    @ModifyExpressionValue(
         method = "attack(Lnet/minecraft/world/entity/Entity;)V",
         at = @At(
             value = "INVOKE",
@@ -20,8 +17,7 @@ public abstract class PlayerAttackMixin {
         require = 1,
         expect = 2
     )
-    private float enchantmentDamageMultiplied$scaleBonus(ItemStack stack, MobType mobType) {
-        float original = EnchantmentHelper.getDamageBonus(stack, mobType);
+    private float enchantmentDamageMultiplied$scaleBonus(float original) {
         return EnchantmentDamageScaler.scaleEnchantmentDamage((Player) (Object) this, original);
     }
 }
